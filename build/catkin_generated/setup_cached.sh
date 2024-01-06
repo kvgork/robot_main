add91ba1662f33dbe7a0630a6c5324f8ea4cde08
Koen <gorkom.projects@gmail.com>
2024-01-06 13:47:49 +0100

    initial catkin_make

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/koen/workspaces/robot_main/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:/home/koen/anaconda3/bin:/home/koen/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/koen/workspaces/robot_main/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/koen/workspaces/robot_main/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/koen/workspaces/robot_main/src:$ROS_PACKAGE_PATH"
